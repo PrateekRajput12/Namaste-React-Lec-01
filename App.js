ce50f315929dc81e774d67f39e814982cbeb8c1c
PrateekRajput12 <ficredinn@gmail.com>
2024-05-21 11:02:13 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+const h = React.createElement("h1",{},[
+React.createElement("p",{},"ky haal h "),
+React.createElement("h2",{},"Ram ram ji")]
+
+)
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(h)
